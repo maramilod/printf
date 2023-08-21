b413440fb57e5e2ac47a27427c1f77762cbe1cba
maramilod <marammilod03@gmail.com>
2023-08-21 11:42:22 -0700

    add

--- ml.c
+++ ml.c
@@ -31,7 +31,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
