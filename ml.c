06fa4760f11f220c6174d4b0e55fab9ff86023af
lujain <lujainm.com>
2023-08-22 06:59:21 -0700

    buffer and width &pre &flags

--- ml.c
+++ ml.c
@@ -25,8 +25,8 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i, cou = 0, j = 0;
-	char buffer[BUF_OF];
-	int flags, width, precision, lenght, buf_i = 0;
+	char buf[BUF_OF];
+	int fla, wid, pre, lenght, buf_i = 0;
 
 	if (format == NULL)
 		return (-1);
@@ -35,13 +35,13 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			main_print(buffer, &buf_i);
-			width = wawa(format, &i, args);
-			flags = fofo(format, &i);
+			main_print(buf, &buf_i);
+			wid = wawa(format, &i, args);
+			fla = fofo(format, &i);
 			lenght = lolo(format, &i);
-			precision = prere(format, &i, args);
+			pre = prere(format, &i, args);
 			i++;
-			j = all(format, &i, args, buffer, width, flags, precision, lenght);
+			j = all(format, &i, args, buf, wid, fla, pre, lenght);
 			if (j == -1)
 				return (-1);
 
@@ -49,13 +49,13 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			buffer[buf_i++] = format[i];
+			buf[buf_i++] = format[i];
 			if (buf_i == BUF_OF)
-				main_print(buffer, &buf_i);
+				main_print(buf, &buf_i);
 			cou++;
 		}
 	}
-	main_print(buffer, &buf_i);
+	main_print(buf, &buf_i);
 	va_end(args);
 	return (cou);
 }
